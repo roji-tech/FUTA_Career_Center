bb0aaba95681e1de16b122d1bd776b72ba1faaa5
roji-tech <rojitech9@gmail.com>
2024-12-11 17:08:10 +0100

    __

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,7 +42,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={""}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <ThemeProvider attribute="class" defaultTheme="light">
           {children}
         </ThemeProvider>
       </body>
