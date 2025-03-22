179f65e07c9892a40a29c728877e5ac278b4726b
roji-tech <rojitech9@gmail.com>
2025-03-22 21:11:00 +0100

    responsiveness

--- components/headers/HomeHeader.tsx
+++ components/headers/HomeHeader.tsx
@@ -1,12 +1,21 @@
-import Image from "next/image";
-import Link from "next/link";
 import React from "react";
+import Link from "next/link";
+
+import { User, UserPlus } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export const LanndingPageHeader = () => {
   return (
     <div className="w-full h-[90px] px-[5%] bg-[#f1b807] justify-between items-center inline-flex">
       <img src={"/logo.svg"} />
-
       <div className="max-sm:hidden justify-start items-center gap-2.5 flex">
         <Link
           href={"/login"}
@@ -21,9 +30,34 @@ export const LanndingPageHeader = () => {
           className="px-10 py-3 bg-[#086cb4] rounded justify-center items-center gap-2.5 flex"
         >
           <span className="whitespace-nowrap text-center text-[#f0f0f1] text-xl font-bold font-['Eudoxus Sans']">
-            SIgn Up
+            Sign Up
           </span>
         </Link>
+      </div>
+
+      <div className="sm:hidden">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline">Menu</Button>
+          </DropdownMenuTrigger>
+
+          <DropdownMenuContent className="w-56">
+            <DropdownMenuGroup>
+              <DropdownMenuItem asChild>
+                <Link href="/login">
+                  <User className="mr-2 h-4 w-4" />
+                  <span>Log In</span>
+                </Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem asChild>
+                <Link href="/register">
+                  <UserPlus className="mr-2 h-4 w-4" />
+                  <span>Sign Up</span>
+                </Link>
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </div>
   );
